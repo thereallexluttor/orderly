020bd3623aee7cb133f4a7583666ffed63186b41
Hedinyer Perucho <alucardperucho@gmail.com>
2024-07-01 20:13:26 +0200

    user chats fixes

--- lib/homepage/UserChats/UserChats.dart
+++ lib/homepage/UserChats/UserChats.dart
@@ -9,37 +9,27 @@ class ChatInfoScreen extends StatefulWidget {
 }
 
 class _ChatInfoScreenState extends State<ChatInfoScreen> {
-  TextEditingController _searchController = TextEditingController();
   String _searchText = "";
-
-  @override
-  void initState() {
-    super.initState();
-    _searchController.addListener(_onSearchTextChanged);
-  }
-
-  @override
-  void dispose() {
-    _searchController.removeListener(_onSearchTextChanged);
-    _searchController.dispose();
-    super.dispose();
-  }
-
-  _onSearchTextChanged() {
-    setState(() {
-      _searchText = _searchController.text;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chats'),
+        title: const Text(
+          'Chats',
+          style: TextStyle(fontFamily: "Poppins", fontSize: 15),
+        ),
         centerTitle: true,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(4.0), // Altura de la línea
+          child: Container(
+            color: Colors.purple, // Color de la línea
+            height: 4.0, // Grosor de la línea
+          ),
+        ),
       ),
-      body: FutureBuilder<DocumentSnapshot>(
-        future: _getUserChatInfo(),
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: _getUserChatInfoStream(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
@@ -54,37 +44,27 @@ class _ChatInfoScreenState extends State<ChatInfoScreen> {
             }
             var chatInfoMap = chatInfo['chatInfo'] as Map<String, dynamic>;
 
-            // Convertimos y ordenamos las entradas por la variable `hora`
+            // Convertimos y ordenamos las entradas por la variable hora
             var sortedChatInfo = chatInfoMap.entries.toList();
             sortedChatInfo.sort((a, b) {
-              var aTime = (a.value['hora'] as Timestamp).toDate();
-              var bTime = (b.value['hora'] as Timestamp).toDate();
+              var aTime = a.value['hora'] != null
+                  ? (a.value['hora'] as Timestamp).toDate()
+                  : DateTime.now();
+              var bTime = b.value['hora'] != null
+                  ? (b.value['hora'] as Timestamp).toDate()
+                  : DateTime.now();
               return bTime.compareTo(aTime); // Orden descendente
             });
 
             // Filtramos los resultados en función del texto de búsqueda
             var filteredChatInfo = sortedChatInfo.where((entry) {
               var value = entry.value as Map<String, dynamic>;
-              var nombre = value['nombre'] as String;
+              var nombre = value['nombre'] as String? ?? "";
               return nombre.toLowerCase().contains(_searchText.toLowerCase());
             }).toList();
 
             return Column(
               children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 10, left: 10, right: 10, bottom: 10),
-                  child: TextField(
-                    controller: _searchController,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.search),
-                      hintText: "Search",
-                      contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                  ),
-                ),
                 Expanded(
                   child: ListView.builder(
                     itemCount: filteredChatInfo.length,
@@ -92,53 +72,62 @@ class _ChatInfoScreenState extends State<ChatInfoScreen> {
                       var entry = filteredChatInfo[index];
                       var key = entry.key;
                       var value = entry.value as Map<String, dynamic>;
-                      var messageTime = (value['hora'] as Timestamp).toDate();
+                      var messageTime = value['hora'] != null
+                          ? (value['hora'] as Timestamp).toDate()
+                          : DateTime.now();
 
-                      return Card(
-                        margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: ListTile(
-                          contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-                          leading: CircleAvatar(
-                            radius: 25,
-                            backgroundImage: NetworkImage(value['foto_producto']),
-                          ),
-                          title: Text(
-                            value['nombre'],
-                            style: TextStyle(
-                              fontFamily: "Poppins",
-                              fontSize: 10,
-                              fontWeight: FontWeight.bold,
+                      return Column(
+                        children: [
+                          Card(
+                            elevation: 0,
+                            margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
                             ),
-                          ),
-                          subtitle: Text(
-                            value['mensaje'],
-                            style: TextStyle(
-                              fontFamily: "Poppins",
-                              fontSize: 14,
-                              color: Colors.grey[600],
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          trailing: Text(
-                            "${messageTime.hour}:${messageTime.minute.toString().padLeft(2, '0')}",
-                            style: TextStyle(
-                              fontFamily: "Poppins",
-                              fontSize: 12,
-                              color: Colors.grey[600],
-                            ),
-                          ),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ProductChat(value),
+                            child: ListTile(
+                              contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                              leading: CircleAvatar(
+                                radius: 25,
+                                backgroundImage: NetworkImage(value['foto_producto']),
                               ),
-                            );
-                          },
-                        ),
+                              title: Text(
+                                value['nombre'] ?? '',
+                                style: TextStyle(
+                                  fontFamily: "Poppins",
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              subtitle: Text(
+                                value['mensaje'] ?? '',
+                                style: TextStyle(
+                                  fontFamily: "Poppins",
+                                  fontSize: 14,
+                                  color: Colors.grey[600],
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              trailing: Text(
+                                "${messageTime.hour}:${messageTime.minute.toString().padLeft(2, '0')}",
+                                style: TextStyle(
+                                  fontFamily: "Poppins",
+                                  fontSize: 12,
+                                  color: Colors.grey[600],
+                                ),
+                              ),
+                              
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => ProductChat(value),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                          Divider(indent: 90,height: 3,), // Divider al final de cada Card
+                        ],
                       );
                     },
                   ),
@@ -151,7 +140,7 @@ class _ChatInfoScreenState extends State<ChatInfoScreen> {
     );
   }
 
-  Future<DocumentSnapshot> _getUserChatInfo() async {
+  Stream<DocumentSnapshot> _getUserChatInfoStream() {
     User? user = FirebaseAuth.instance.currentUser;
     if (user != null) {
       var userDocRef = FirebaseFirestore.instance
@@ -159,7 +148,7 @@ class _ChatInfoScreenState extends State<ChatInfoScreen> {
           .doc('Users')
           .collection('users')
           .doc(user.uid);
-      return await userDocRef.get();
+      return userDocRef.snapshots();
     }
     throw FirebaseAuthException(
       code: 'USER_NOT_LOGGED_IN',
