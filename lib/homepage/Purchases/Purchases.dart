c7d9fe038c3df6050034a922bb14a1ea598a6719
Hedinyer Perucho <alucardperucho@gmail.com>
2024-07-09 23:14:11 +0200

    shopping cart and purchases fix

--- lib/homepage/Purchases/Purchases.dart
+++ lib/homepage/Purchases/Purchases.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Purchases extends StatefulWidget {
   const Purchases({super.key});
@@ -47,6 +48,36 @@ class _PurchasesState extends State<Purchases> with SingleTickerProviderStateMix
     return null;
   }
 
+  Map<String, dynamic> _calculateMetrics(Map<String, dynamic> purchaseData) {
+    double totalSpent = 0.0;
+    Map<String, int> productCount = {};
+    DateTime now = DateTime.now();
+    DateTime oneWeekAgo = now.subtract(Duration(days: 7));
+
+    purchaseData.forEach((key, value) {
+      if (value['fecha_compra'] != null && value['total_pagar'] != null && value['cantidad'] != null && value['nombre_producto'] != null) {
+        DateTime date = DateTime.parse(value['fecha_compra']);
+        double total = (value['total_pagar'] as num).toDouble();
+        int cantidad = value['cantidad'] as int;
+
+        if (date.isAfter(oneWeekAgo)) {
+          totalSpent += total;
+
+          if (productCount.containsKey(value['nombre_producto'])) {
+            productCount[value['nombre_producto']] = productCount[value['nombre_producto']]! + cantidad;
+          } else {
+            productCount[value['nombre_producto']] = cantidad;
+          }
+        }
+      }
+    });
+
+    return {
+      'totalSpent': totalSpent,
+      'productCount': productCount,
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,7 +90,7 @@ class _PurchasesState extends State<Purchases> with SingleTickerProviderStateMix
         ),
       ),
       body: Container(
-        color: Colors.white, // Fondo blanco para la pantalla
+        color: Colors.white,
         child: FadeTransition(
           opacity: _animation!,
           child: FutureBuilder<Map<String, dynamic>?>(
@@ -77,53 +108,96 @@ class _PurchasesState extends State<Purchases> with SingleTickerProviderStateMix
                 return Center(child: Text('No has realizado ninguna compra.'));
               }
 
-              return ListView.builder(
-                itemCount: purchaseData.length,
-                itemBuilder: (context, index) {
-                  String key = purchaseData.keys.elementAt(index);
-                  Map<String, dynamic> item = purchaseData[key];
+              Map<String, dynamic> metrics = _calculateMetrics(purchaseData);
+              double totalSpent = metrics['totalSpent'];
+              Map<String, int> productCount = metrics['productCount'];
 
-                  return Card(
-                    elevation: 0,
-                    color: Colors.white, // Fondo blanco para las tarjetas
-                    margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10), // Tarjetas más delgadas
-                    child: Padding(
-                      padding: EdgeInsets.all(5), // Ajustar padding para hacer las tarjetas más delgadas
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ListTile(
-                            leading: ClipRRect(
-                              borderRadius: BorderRadius.circular(8.0), // Redondear bordes de las imágenes
-                              child: Image.network(
-                                item['foto_producto'],
-                                width: 50,
-                                height: 50,
-                                fit: BoxFit.cover,
+              List<MapEntry<String, int>> sortedProducts = productCount.entries.toList()
+                ..sort((a, b) => b.value.compareTo(a.value));
+
+              final numberFormat = NumberFormat('#,##0', 'es_ES');
+
+              return ListView(
+                padding: EdgeInsets.all(16),
+                children: [
+                  Text(
+                    'Estadísticas de la última semana:',
+                    style: TextStyle(fontFamily: "Poppins", fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Monto total gastado: COP ${numberFormat.format(totalSpent)}',
+                    style: TextStyle(fontFamily: "Poppins", fontSize: 12),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Productos más comprados:',
+                    style: TextStyle(fontFamily: "Poppins", fontSize: 12, fontWeight: FontWeight.bold),
+                  ),
+                  ...sortedProducts.map((entry) => ListTile(
+                        title: Text(
+                          entry.key,
+                          style: TextStyle(fontFamily: "Poppins", fontSize: 12),
+                        ),
+                        trailing: Text(
+                          'Cantidad: ${entry.value}',
+                          style: TextStyle(fontFamily: "Poppins", fontSize: 12),
+                        ),
+                      )),
+                  Divider(),
+                  Text(
+                    'Detalle de compras:',
+                    style: TextStyle(fontFamily: "Poppins", fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                  ...purchaseData.entries.map((entry) {
+                    Map<String, dynamic> item = entry.value;
+
+                    if (item['nombre_producto'] != null && item['cantidad'] != null && item['total_pagar'] != null && item['foto_producto'] != null && item['fecha_compra'] != null) {
+                      return Card(
+                        elevation: 0,
+                        color: Colors.white,
+                        margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ListTile(
+                                leading: ClipRRect(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  child: Image.network(
+                                    item['foto_producto'],
+                                    width: 50,
+                                    height: 50,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                title: Text(
+                                  item['nombre_producto'],
+                                  style: TextStyle(fontFamily: "Poppins", fontSize: 12, fontWeight: FontWeight.bold),
+                                ),
+                                subtitle: Text(
+                                  'X${item['cantidad']}',
+                                  style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold, fontSize: 12),
+                                ),
+                                trailing: Text('COP ${numberFormat.format(item['total_pagar'])}'),
                               ),
-                            ),
-                            title: Text(
-                              item['nombre_producto'],
-                              style: TextStyle(fontFamily: "Poppins", fontSize: 13, fontWeight: FontWeight.bold),
-                            ),
-                            subtitle: Text(
-                              'X${item['cantidad']}',
-                              style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold, fontSize: 14),
-                            ),
-                            trailing: Text('COP ${item['total_pagar']}'),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 16.0, top: 8.0),
+                                child: Text(
+                                  'Fecha de Compra: ${item['fecha_compra']}',
+                                  style: TextStyle(fontFamily: "Poppins", fontSize: 12, color: Colors.grey),
+                                ),
+                              ),
+                            ],
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 16.0, top: 8.0),
-                            child: Text(
-                              'Fecha de Compra: ${item['fecha_compra']}',
-                              style: TextStyle(fontFamily: "Poppins", fontSize: 12, color: Colors.grey),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
+                        ),
+                      );
+                    } else {
+                      return SizedBox.shrink(); // Retorna un widget vacío si algún valor es nulo
+                    }
+                  }).toList(),
+                ],
               );
             },
           ),
--- lib/homepage/shopping_cart/shopping_cart.dart
+++ lib/homepage/shopping_cart/shopping_cart.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:confetti/confetti.dart';
 
 class Shopping_Cart extends StatefulWidget {
   const Shopping_Cart({super.key});
@@ -15,6 +16,7 @@ class Shopping_Cart extends StatefulWidget {
 class _Shopping_CartState extends State<Shopping_Cart> with SingleTickerProviderStateMixin {
   AnimationController? _controller;
   Animation<double>? _animation;
+  late ConfettiController _confettiController;
 
   @override
   void initState() {
@@ -25,11 +27,13 @@ class _Shopping_CartState extends State<Shopping_Cart> with SingleTickerProvider
     );
     _animation = CurvedAnimation(parent: _controller!, curve: Curves.easeIn);
     _controller?.forward();
+    _confettiController = ConfettiController(duration: const Duration(seconds: 2));
   }
 
   @override
   void dispose() {
     _controller?.dispose();
+    _confettiController.dispose();
     super.dispose();
   }
 
@@ -132,105 +136,131 @@ class _Shopping_CartState extends State<Shopping_Cart> with SingleTickerProvider
     }
 
     setState(() {});
+    _confettiController.play();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
+        surfaceTintColor: Colors.white,
         title: Text(
           'Tu carrito de compras. 🛒👀🧐',
           style: TextStyle(fontFamily: "Poppins", fontSize: 13),
         ),
       ),
-      body: FadeTransition(
-        opacity: _animation!,
-        child: FutureBuilder<Map<String, dynamic>?>(
-          future: _fetchCartData(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            }
-            if (!snapshot.hasData || snapshot.data == null) {
-              return Center(child: Text('No se encontraron datos del carrito.'));
-            }
+      body: Stack(
+        children: [
+          FadeTransition(
+            opacity: _animation!,
+            child: FutureBuilder<Map<String, dynamic>?>(
+              future: _fetchCartData(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                if (!snapshot.hasData || snapshot.data == null) {
+                  return Center(child: Text('No se encontraron datos del carrito.'));
+                }
 
-            Map<String, dynamic> cartData = snapshot.data!['carrito_compra'] ?? {};
-            if (cartData.isEmpty) {
-              return Center(child: Text('El carrito está vacío.'));
-            }
+                Map<String, dynamic> cartData = snapshot.data!['carrito_compra'] ?? {};
+                if (cartData.isEmpty) {
+                  return Center(child: Text('El carrito está vacío.'));
+                }
 
-            double totalPrice = cartData.values.fold(0.0, (sum, item) => sum + item['total_pagar']);
+                double totalPrice = cartData.values.fold(0.0, (sum, item) => sum + item['total_pagar']);
 
-            return Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: cartData.length,
-                    itemBuilder: (context, index) {
-                      String key = cartData.keys.elementAt(index);
-                      Map<String, dynamic> item = cartData[key];
+                String formatCurrency(int amount) {
+                  final formatter = NumberFormat('#,##0', 'es_CO');
+                  return formatter.format(amount);
+                }
 
-                      return Dismissible(
-                        key: Key(key),
-                        direction: DismissDirection.endToStart,
-                        onDismissed: (direction) async {
-                          await _deleteItemFromCart(key, item);
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text('${item['nombre_producto']} eliminado del carrito')),
+                return Column(
+                  children: [
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: cartData.length,
+                        itemBuilder: (context, index) {
+                          String key = cartData.keys.elementAt(index);
+                          Map<String, dynamic> item = cartData[key];
+
+                          return Dismissible(
+                            key: Key(key),
+                            direction: DismissDirection.endToStart,
+                            onDismissed: (direction) async {
+                              await _deleteItemFromCart(key, item);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text('${item['nombre_producto']} eliminado del carrito')),
+                              );
+                            },
+                            background: Container(
+                              color: Colors.red,
+                              alignment: Alignment.centerRight,
+                              padding: EdgeInsets.symmetric(horizontal: 20),
+                              child: Icon(Icons.delete, color: Colors.white),
+                            ),
+                            child: ListTile(
+                              leading: Image.network(item['foto_producto'],
+                              width: 50,
+                              height: 50,
+                              fit: BoxFit.fill,),
+                              title: Text(
+                                item['nombre_producto'],
+                                style: TextStyle(fontFamily: "Poppins", fontSize: 11),
+                              ),
+                              subtitle: Text(
+                                'X${item['cantidad']}',
+                                style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold, fontSize: 10),
+                              ),
+                              trailing: Text('COP ${formatCurrency(item['total_pagar'])}'),
+                            ),
                           );
                         },
-                        background: Container(
-                          color: Colors.red,
-                          alignment: Alignment.centerRight,
-                          padding: EdgeInsets.symmetric(horizontal: 20),
-                          child: Icon(Icons.delete, color: Colors.white),
-                        ),
-                        child: ListTile(
-                          leading: Image.network(item['foto_producto']),
-                          title: Text(
-                            item['nombre_producto'],
-                            style: TextStyle(fontFamily: "Poppins", fontSize: 11),
-                          ),
-                          subtitle: Text(
-                            'X${item['cantidad']}',
-                            style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold, fontSize: 10),
-                          ),
-                          trailing: Text('COP ${item['total_pagar']}'),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Total: COP $totalPrice',
-                        style: TextStyle(fontFamily: "Poppins", fontSize: 16, fontWeight: FontWeight.bold),
                       ),
-                      SizedBox(height: 10),
-                      ElevatedButton(
-                        onPressed: _completePurchase,
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size(double.infinity, 50),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Total: COP ${formatCurrency(totalPrice.toInt())}',
+                            style: TextStyle(fontFamily: "Poppins", fontSize: 14, fontWeight: FontWeight.normal),
                           ),
-                        ),
-                        child: Text(
-                          'Ir a pagar',
-                          style: TextStyle(fontFamily: "Poppins", fontSize: 16),
-                        ),
+                          SizedBox(height: 10),
+                          ElevatedButton(
+                            onPressed: _completePurchase,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.purple,
+                              minimumSize: Size(double.infinity, 50),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(6),
+                              ),
+                            ),
+                            child: Text(
+                              'Ir a pagar',
+                              style: TextStyle(fontFamily: "Poppins", fontSize: 14, color: Colors.white,  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-              ],
-            );
-          },
-        ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: ConfettiWidget(
+              confettiController: _confettiController,
+              blastDirectionality: BlastDirectionality.explosive,
+              shouldLoop: false,
+              colors: const [Colors.red, Colors.blue, Colors.green, Colors.yellow], // confetti colors
+            ),
+          ),
+        ],
       ),
     );
   }
